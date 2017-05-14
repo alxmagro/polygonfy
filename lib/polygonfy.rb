7d25ea55e5046950ba6fdd36ca163510ca8b1db3
Alexandre Magro <alexandremagro@live.com>
2017-05-14 01:48:04 -0300

    Create class Polygon

--- lib/polygonfy.rb
+++ lib/polygonfy.rb
@@ -1,29 +1,13 @@
 require "polygonfy/version"
 require "polygonfy/Point"
-require "nokogiri"
+require "polygonfy/Polygon"
 
 module Polygonfy
-  MARGIN = 12
-  STYLES = {
-    polygon: "fill:#4f9bff; stroke:#216cce; stroke-width:1; opacity:0.5",
-    circle: "fill:orange"
-  }
-
-  def self.point_title(array, i)
-    prev_point = array[i - 1].id
-    next_point = array[(i + 1) % array.size].id
-    curr_point = "#{array[i].x}, #{array[i].y}"
-
-    "#{prev_point} (#{curr_point}) #{next_point}"
-  end
-
   def self.run
     if ARGV.length == 0
       puts "Use: polygonfy filename [id,x,y] [id,x,y] [..]"
       return
     end
-
-    filename = ARGV[0]
 
     points = ARGV.length > 1 ? ARGV.drop(1) : STDIN.readline.split(' ')
 
@@ -32,31 +16,10 @@ module Polygonfy
       Point.new(id, x.to_i, y.to_i)
     end
 
-    xmlns  = "http://www.w3.org/2000/svg"
-    width  = points.map(&:x).max + (2 * MARGIN)
-    height = points.map(&:y).max + (2 * MARGIN)
+    polygon = Polygon.new(points)
 
-    polygon_points = points.map { |p| "#{p.x + MARGIN},#{p.y + MARGIN}" }.join(' ')
-
-    builder = Nokogiri::XML::Builder.new do |xml|
-      xml.svg(xmlns: xmlns, width: width, height: height) {
-        xml.polygon(style: STYLES[:polygon], points: polygon_points)
-        xml.g {
-          points.each_with_index do |p, i|
-            xml.circle(style: STYLES[:circle], cx: p.x + MARGIN, cy: p.y + MARGIN, r: "12")
-            xml.text_(x: p.x + MARGIN, y: p.y + 5 + MARGIN, 'text-anchor': 'middle') {
-              xml.text(p.id)
-              xml.title {
-                xml.text(point_title(points, i))
-              }
-            }
-          end
-        }
-      }
-    end
-
-    File.open(filename, 'w') do |f|
-      f.write(builder.to_xml)
+    File.open(ARGV[0], 'w') do |f|
+      f.write(polygon.to_svg)
     end
   end
 end
---
+++ lib/polygonfy/Polygon.rb
@@ -0,0 +1,59 @@
+require "nokogiri"
+
+module Polygonfy
+  class Polygon < Struct.new(:points)
+    MARGIN = 12
+    STYLES = {
+      polygon: "fill:#4f9bff; stroke:#216cce; stroke-width:1; opacity:0.5",
+      circle: "fill:orange"
+    }
+
+    def width
+      points.map(&:x).max + (2 * MARGIN)
+    end
+
+    def height
+      points.map(&:y).max + (2 * MARGIN)
+    end
+
+    def polygon_area
+      points.map { |p| "#{p.x + MARGIN},#{height - (p.y + MARGIN)}" }.join(' ')
+    end
+
+    def point_title(i)
+      prev_point = points[i - 1].id
+      next_point = points[(i + 1) % points.size].id
+      curr_point = "(#{points[i].x}, #{points[i].y})"
+
+      "#{prev_point} #{curr_point} #{next_point}"
+    end
+
+    def point_x(point)
+      point.x + MARGIN
+    end
+
+    def point_y(point)
+      height - (point.y + MARGIN)
+    end
+
+    def to_svg
+      Nokogiri::XML::Builder.new do |xml|
+        xml.svg(xmlns: "http://www.w3.org/2000/svg", width: width, height: height) {
+          xml.polygon(style: STYLES[:polygon], points: polygon_area)
+          xml.g {
+            points.each_with_index do |p, i|
+              xml.circle(style: STYLES[:circle], cx: point_x(p), cy: point_y(p), r: "12")
+              xml.text_(x: point_x(p), y: point_y(p) + 5, 'text-anchor': 'middle') {
+                xml.text(p.id)
+                xml.title {
+                  xml.text(point_title(i))
+                }
+              }
+            end
+          }
+        }
+      end
+    end
+
+  end
+end
